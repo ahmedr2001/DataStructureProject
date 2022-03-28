909526012c56dd1962c0f80bac1f918343986235
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-03-28 23:23:08 +0200

    Truck done

--- DataStructureProject/Truck.cpp
+++ DataStructureProject/Truck.cpp
@@ -1,1 +1,77 @@
 #include "Truck.h"
+
+Truck::Truck(Type t, int cap, int ct, int s)
+{
+	loaded = 0;
+	set_Type(t);
+	set_Capacity(cap);
+	set_Check_Time(ct);
+	set_Speed(s);
+}
+
+void Truck::add_Cargo(Cargo* c)
+{
+	if (loaded<Capacity)
+	{
+		cargolist->push(c);
+		loaded++;
+	}
+}
+
+void Truck::set_Capacity(int cap)
+{
+	Capacity = cap;
+}
+
+void Truck::set_Type(Type t)
+{
+	trucktype = t;
+}
+
+void Truck::set_Check_Time(int ct)
+{
+	check_time = ct;
+}
+
+void Truck::set_Speed(int s)
+{
+	speed = s;
+}
+
+void Truck::set_DI()
+{
+	// write eq here
+}
+
+int Truck::get_Capacity()
+{
+	return Capacity;
+}
+
+Type Truck::get_Type()
+{
+	return trucktype;
+}
+
+int Truck::get_Check_Time()
+{
+	return check_time;
+}
+
+int Truck::get_Speed()
+{
+	return speed;
+}
+
+int Truck::get_DI()
+{
+	return DI;
+}
+
+void Truck::LoadFromStreamFile()
+{
+}
+
+void Truck::OutToStreamFile()
+{
+}
--- DataStructureProject/Truck.h
+++ DataStructureProject/Truck.h
@@ -6,12 +6,28 @@ class Truck
 {
 private:
 	stack<Cargo*>* cargolist; //list of cargo pointers
-	int TC; // truck capacity
+	int Capacity; // truck capacity
+	int loaded; // cargos count
 	int check_time; //maintenance time
 	int num_of_journey; // truck needs to perform maintainance after finishing n delivery journeys
 	int speed; // speed in km/h
 	int DI; // delivery interval
-	//trucktype tt;
+	Type trucktype; // truck type (vip-normal-special)
 public:
+	Truck(Type t,int cap,int ct,int s); // set type-capacity-check time-speed of truck
+	void add_Cargo(Cargo* c); // add cargo to cargolist
+	void set_Capacity(int cap); // set max capacity of truck
+	void set_Type(Type t); // set type of truck (vip-normal-special)
+	void set_Check_Time(int ct); // set maintainance time
+	void set_Speed(int s); // set truck speed in km/h
+	void set_DI(); // set delivery interval
+	
+	int get_Capacity(); // get max capacity of truck
+	Type get_Type(); // get type of truck (vip-normal-special)
+	int get_Check_Time(); // get maintainance time
+	int get_Speed(); // get truck speed in km/h
+	int get_DI(); // get delivery interval
 
+	void LoadFromStreamFile(); // load from .txt file
+	void OutToStreamFile(); // output to console
 };
