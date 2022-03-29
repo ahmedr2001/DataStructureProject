5e01ccab0979e3e9b566b89538b8aa4ec5a276cb
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-03-29 15:21:54 +0200

    part of load

--- DataStructureProject/Company.cpp
+++ DataStructureProject/Company.cpp
@@ -1,5 +1,28 @@
 #include "Company.h"
 void Company::LoadTrucksAndEventsData(string filename)
 {
+	ifstream inFile(filename + ".txt", ios::in);
 
+	if (inFile.is_open())
+	{
+		inFile >> Normal_Truck_Num;
+		inFile >> Special_Truck_Num;
+		inFile >> Vip_Truck_Num;
+
+		inFile >> Normal_Truck_Speed;
+		inFile >> Special_Truck_Speed;
+		inFile >> Vip_Truck_Speed;
+
+		inFile >> Normal_Truck_Cap;
+		inFile >> Special_Truck_Cap;
+		inFile >> Vip_Truck_Cap;
+
+		inFile >> Normal_journey_Num;
+		inFile >> Special_journey_Num;
+		inFile >> Vip_journey_Num;
+
+		inFile >> AutoP;
+		inFile >> MaxW;
+
+		inFile >> Event_Num;
 }
--- DataStructureProject/Company.h
+++ DataStructureProject/Company.h
@@ -3,10 +3,26 @@
 #include "Truck.h"
 #include "Event.h"
 #include "priority_queue.h"
+#include <fstream>
 
 class Company
 {
 protected:
+	int Normal_Truck_Num;
+	int Special_Truck_Num;
+	int Vip_Truck_Num;
+	int Normal_Truck_Speed;
+	int Special_Truck_Speed;
+	int Vip_Truck_Speed;
+	int Normal_Truck_Cap;
+	int Special_Truck_Cap;
+	int Vip_Truck_Cap;
+	int Normal_journey_Num;
+	int Special_journey_Num;
+	int Vip_journey_Num;
+	int AutoP;
+	int MaxW;
+	int Event_Num;
 	queue<Event*>* eventList;
 	queue<Cargo*>* normalWaitingList;
 	queue<Cargo*>* specialWaitingList;
@@ -21,6 +37,7 @@ protected:
 	queue<Truck*>* normalTruckList;
 	queue<Truck*>* specialTruckList;
 public:
+
 	void LoadTrucksAndEventsData(string filename);
 };
 
