9946763426d92dea8b3c19b59a69dd9934ec653c
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-03-29 15:50:00 +0200

    read file done

--- DataStructureProject/Company.cpp
+++ DataStructureProject/Company.cpp
@@ -25,4 +25,34 @@ void Company::LoadTrucksAndEventsData(string filename)
 		inFile >> MaxW;
 
 		inFile >> Event_Num;
+
+		Event* e;
+		for (int i = 0; i < Event_Num; i++)
+		{
+			inFile >> Event_Type;
+			if (Event_Type=='R')
+			{
+				inFile >> Cargo_Type;
+				inFile >> Event_Time;
+				inFile >> Cargo_ID;
+				inFile >> Cargo_Dist;
+				inFile >> Load_Time;
+				inFile >> Cargo_cost;
+				e = new Prepare_Event();
+			}
+			else if (Event_Type == 'X')
+			{
+				inFile >> Event_Time;
+				inFile >> Cargo_ID;
+				e = new Prepare_Event();
+			}
+			else if (Event_Type == 'P')
+			{
+				inFile >> Event_Time;
+				inFile >> Cargo_ID;
+				inFile >> Extra_Money;
+				e = new Prepare_Event();
+			}
+			eventList->enqueue(e);
+		}
 }
--- DataStructureProject/Company.h
+++ DataStructureProject/Company.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "Time.h"
 #include "Truck.h"
-#include "Event.h"
+#include "Cancel_Event.h"
+#include "Prepare_Event.h"
+#include "Promote_Event.h"
 #include "priority_queue.h"
 #include <fstream>
 
@@ -11,18 +13,32 @@ protected:
 	int Normal_Truck_Num;
 	int Special_Truck_Num;
 	int Vip_Truck_Num;
+
 	int Normal_Truck_Speed;
 	int Special_Truck_Speed;
 	int Vip_Truck_Speed;
+
 	int Normal_Truck_Cap;
 	int Special_Truck_Cap;
 	int Vip_Truck_Cap;
+
 	int Normal_journey_Num;
 	int Special_journey_Num;
 	int Vip_journey_Num;
+
 	int AutoP;
 	int MaxW;
+
 	int Event_Num;
+	char Event_Type;
+	Type Cargo_Type;
+	Time Event_Time;
+	int Cargo_ID;
+	int Cargo_Dist;
+	int Load_Time;
+	int Cargo_cost;
+	int Extra_Money;
+
 	queue<Event*>* eventList;
 	queue<Cargo*>* normalWaitingList;
 	queue<Cargo*>* specialWaitingList;
