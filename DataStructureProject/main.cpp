ae46cd3fb4f064dc1ea0568863ba14c401edbf98
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2022-04-02 15:14:35 +0200

    a

--- DataStructureProject/main.cpp
+++ DataStructureProject/main.cpp
@@ -6,16 +6,25 @@
 using namespace std;
 
 int main() {
-	Time t;
+	/*Time t;
 	Company* c1 = new Company();
 	c1->LoadTrucksAndEventsData("test");
 	while (t.get_Day()!=14||t.get_Hour()!=12)
 	{
 		c1->Execute_Events(t);
-		Sleep(5);
+		Sleep(100);
 		t++;
 		c1->printathing();
-	}
-	
+	}*/
+	Time d2(12,3);
+	priority_queue<Cargo*>* test = new priority_queue<Cargo*>;
+	Cargo* c1 = new Cargo(d2, 500, 4, VIP, 300, 250);
+	Cargo* c2 = new Cargo(d2, 550, 1, VIP, 150, 500);
+	Cargo* c3 = new Cargo(d2, 520, 3, VIP, 200, 150);
+
+	test->enqueue(c2);
+	test->enqueue(c3);
+	test->enqueue(c1);
+
 	return 0;
 }
