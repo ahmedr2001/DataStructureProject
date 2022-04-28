4927e881457b0b60aa943c2935c1bfb3843d624d
Ahmed Abdelatty <ahmedradalla2001@gmail.com>
2022-04-28 14:53:49 +0200

    ز

--- DataStructureProject/priority_queue.h
+++ DataStructureProject/priority_queue.h
@@ -5,7 +5,7 @@ template <class T>
 class priority_queue : public queue<T>
 {
 public:
-	void enqueue(T* data, int distanceWeight, int costWeight, int prepTimeWeight)
+	void enqueue(T* data, int distanceWeight=1, int costWeight=1, int prepTimeWeight=-1)
 	{
 		linkedlist<T>* Q = this->q;
 		Q->addWithPriority(data, distanceWeight, costWeight, prepTimeWeight);
