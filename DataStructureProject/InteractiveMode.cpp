ec3977ba744b633273a8461fe12e9bb862be1479
IbraheimTarek <77370509+IbraheimTarek@users.noreply.github.com>
2022-05-23 19:52:19 +0200

    blaa

--- DataStructureProject/InteractiveMode.cpp
+++ DataStructureProject/InteractiveMode.cpp
@@ -16,12 +16,18 @@ void InteractiveMode::Simulate(Company* c1, Time& t, UI* uiObject) {
 		c1->AutoPromote(Normal, t);
 		c1->MoveTrucksToCheckup(t);
 		c1->MoveCheckupToAvail(t);
-		if (t.get_Hour() % 5 == 4 && t.get_Hour() >= 5 && t.get_Hour() <= 23) {
-			c1->Moving_WaitingCargo(VIP, t);
+		
+		if (t.get_Hour() >= 5 && t.get_Hour() <= 23) {
+			c1->Load(t);
+			c1->Truck_Loading_Moving(t);
+			/*c1->Moving_WaitingCargo(VIP, t);
 			c1->Moving_WaitingCargo(special, t);
-			c1->Moving_WaitingCargo(Normal, t);
+			c1->Moving_WaitingCargo(Normal, t);*/
 			//c1->Increase_Timers();
 		}
+		c1->Deliver_MovingCargo(Normal, t);
+		c1->Deliver_MovingCargo(special, t);
+		c1->Deliver_MovingCargo(VIP, t);
 		//if (t.get_Hour() % 5 == 4) {
 		//	c1->Deliver_MovingCargo(VIP, t);
 		//	c1->Deliver_MovingCargo(special, t);
